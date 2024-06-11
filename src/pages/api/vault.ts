762439c11698b1e3d1a8f5e05f0c0a5d9274ffa1
Artur Dudek <ejer.priv@wp.pl>
2024-06-11 18:19:22 +0200

    comment breaking code

--- src/pages/api/vault.ts
+++ src/pages/api/vault.ts
@@ -22,36 +22,36 @@ export default withApiMethods({
   POST: withApiValidation(InputSchema, async (req, res) => {
     const { name, value } = req.body;
 
-    const user = getServerSideSession(req);
+    // const user = getServerSideSession(req);
 
-    if (userExists.length === 0) {
-      res.status(400).json(buildErrorResponse("Invalid email or password"));
-      return;
-    }
+    // if (userExists.length === 0) {
+    //   res.status(400).json(buildErrorResponse("Invalid email or password"));
+    //   return;
+    // }
 
-    const passwordCorrect = await compare(password, userExists[0].password);
+    // const passwordCorrect = await compare(password, userExists[0].password);
 
-    if (!passwordCorrect) {
-      res.status(400).json(buildErrorResponse("Invalid email or password"));
-      return;
-    }
+    // if (!passwordCorrect) {
+    //   res.status(400).json(buildErrorResponse("Invalid email or password"));
+    //   return;
+    // }
 
-    // set session cookie
-    const token = jwt.sign({ id: userExists[0].id }, process.env.JWT_SECRET!, {
-      expiresIn: 60 * 60,
-    });
+    // // set session cookie
+    // const token = jwt.sign({ id: userExists[0].id }, process.env.JWT_SECRET!, {
+    //   expiresIn: 60 * 60,
+    // });
 
-    const cookie = serialize("session", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 60 * 60,
-      sameSite: "strict",
-      path: "/",
-    });
+    // const cookie = serialize("session", token, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === "production",
+    //   maxAge: 60 * 60,
+    //   sameSite: "strict",
+    //   path: "/",
+    // });
 
-    console.log(cookie);
+    // console.log(cookie);
 
-    res.setHeader("Set-Cookie", cookie);
-    res.status(200).json(buildResponse(true));
+    // res.setHeader("Set-Cookie", cookie);
+    // res.status(200).json(buildResponse(true));
   }),
 });
