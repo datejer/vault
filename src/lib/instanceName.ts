33f8c4e4d3a5fbf4a09c1d9e7c0770dd24ccfbfe
Artur Dudek <ejer.priv@wp.pl>
2024-06-18 12:49:15 +0200

    fix instance names

--- src/lib/instanceName.ts
+++ src/lib/instanceName.ts
@@ -1,4 +1,7 @@
 const definedInstanceName = process.env.NEXT_PUBLIC_INSTANCE_NAME;
-const deploymentDomain = process.env.NEXT_PUBLIC_VERCEL_URL;
+const env = process.env.NEXT_PUBLIC_VERCEL_ENV;
+const prodUrl = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL;
+const deploymentUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
+const deploymentDomain = env === "production" ? prodUrl : deploymentUrl;
 const instanceName = definedInstanceName || deploymentDomain || "Vault";
 export { instanceName };
