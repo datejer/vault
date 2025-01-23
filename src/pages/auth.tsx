115c433381a025431f4667f81f2d77c832fad5e5
Artur Dudek <ejer.priv@wp.pl>
2025-01-23 13:54:41 +0100

    experiment: wake db from sleep

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,6 +1,9 @@
+import { waitUntil } from "@vercel/functions";
+import { sql } from "drizzle-orm";
 import LoginForm from "@/components/LoginForm";
 import RegisterForm from "@/components/RegisterForm";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { db } from "@/db";
 
 export default function AuthPage(): JSX.Element {
   return (
@@ -19,4 +22,15 @@ export default function AuthPage(): JSX.Element {
       </Tabs>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  try {
+    // Ping the database to wake up from sleep (avoid cold start on login request)
+    waitUntil(db.run(sql`SELECT 1`));
+    return { props: {} };
+  } catch (error) {
+    console.error("Error pinging database:", error);
+    return { props: {} };
+  }
 }
