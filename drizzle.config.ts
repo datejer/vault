c5266fa6e456025654e898568631b224ccc283d8
ejer <ejer.priv@wp.pl>
2025-04-19 23:11:22 +0200

    update drizzle turso syntax

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,11 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
+export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./drizzle/migrations",
-  dialect: "sqlite",
-  driver: "turso",
+  dialect: "turso",
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,
   },
-} satisfies Config;
+});
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,10 @@
-import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 
-const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+export const db = drizzle({
+  connection: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  },
+  schema,
 });
-
-export const db = drizzle(turso, { schema });
